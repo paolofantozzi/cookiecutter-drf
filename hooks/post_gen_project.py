97073d3d6efbed34f3433a80b0723b0389c10921
Paolo Fantozzi <paolo.fantozzi@gmail.com>
2020-04-11 12:12:31 +0200

    Delete docker files only if docker

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -383,7 +383,10 @@ def main():
     if "{{ cookiecutter.use_drf }}".lower() == "n":
         remove_drf_starter_files()
 
-    if "{{ cookiecutter.ssl_mode }}".lower() != "static certificate":
+    if (
+        "{{ cookiecutter.ssl_mode }}".lower() != "static certificate"
+        and "{{ cookiecutter.use_docker }}".lower() == "y"
+    ):
         remove_static_certs()
 
     print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
