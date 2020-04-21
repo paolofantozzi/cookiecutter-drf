a7a27de381ade5fd25aab9a39984f1597c193592
Paolo Fantozzi <paolo.fantozzi@gmail.com>
2020-04-21 15:36:07 +0200

    Local testing script (copy of travis tests)

--- README.rst
+++ README.rst
@@ -19,6 +19,7 @@ Forked from https://github.com/pydanny/cookiecutter-django with the following ch
   - LetsEncrypt -> certificate request and renewal automatically with letsencrypt
   - Static Cert -> use certicate provided (path compose/production/traefik/certs)
 - Make migrations command
+- Added local testing script
 - Api only mode:
 
   - Activated by default (together with drf)
---
+++ tests/test_in_local.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# this is a very simple script that tests the docker configuration for cookiecutter-django
+# it is meant to be run from the root directory of the repository, eg:
+# sh tests/test_docker.sh
+
+set -o errexit
+
+# install test requirements
+pip install -r requirements.txt
+
+# create a cache directory
+mkdir -p .cache/docker
+cd .cache/docker
+
+# create the project using the default settings in cookiecutter.json
+cookiecutter ../../ --no-input --overwrite-if-exists use_docker=y $@
+cd my_awesome_project
+
+docker-compose -f local.yml up --build -d
+
+# run the project's type checks
+docker-compose -f local.yml run django mypy my_awesome_project || docker-compose -f local.yml down -v
+
+# run the project's tests
+docker-compose -f local.yml run django pytest || docker-compose -f local.yml down -v
+
+# return non-zero status code if there are migrations that have not been created
+docker-compose -f local.yml run django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; docker-compose -f local.yml down -v; exit 1; }
+
+# Test support for translations
+docker-compose -f local.yml run django python manage.py makemessages || docker-compose -f local.yml down -v
+
+docker-compose -f local.yml down -v
